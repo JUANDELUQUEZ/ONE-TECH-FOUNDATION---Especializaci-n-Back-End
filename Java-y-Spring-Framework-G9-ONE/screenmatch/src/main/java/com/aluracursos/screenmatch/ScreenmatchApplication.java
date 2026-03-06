7818e81ece03126f85efd7a3b84586ee795f0aee
Juan Deluquez <juandeluquez1123@gmail.com>
2026-03-06 17:28:58 -0600

    Importamos la libreria de CommandLineRunner para poder tener un contrato y poder ejecutar codigo puntual

--- Java-y-Spring-Framework-G9-ONE/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ Java-y-Spring-Framework-G9-ONE/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -9,7 +9,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
-
+// sobrescribimos el método run para ejecutar código al iniciar la aplicación
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hola mundo desde Spring Boot!");
