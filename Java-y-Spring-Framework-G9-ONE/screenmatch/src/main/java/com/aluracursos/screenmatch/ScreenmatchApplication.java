9be1b5d6e8caaa176e053d53e82fc9f3999fb31a
Juan Deluquez <juandeluquez1123@gmail.com>
2026-03-06 18:19:05 -0600

    Obtenemos datos reales de la Api de OBDB
    Sobrescribimos el metodo de run para definir la variable para el soncumo de la api y asi nos muestre en texto plano informacion sobre un apelicula, insurgente,...

--- Java-y-Spring-Framework-G9-ONE/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ Java-y-Spring-Framework-G9-ONE/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aluracursos.screenmatch.service.ConsumoApi;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
@@ -10,8 +12,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 // sobrescribimos el método run para ejecutar código al iniciar la aplicación
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo desde Spring Boot!");
-	}
+@Override
+    public void run(String... args) throws Exception {
+        // 1. Creamos la instancia del servicio usando 'var'
+        var consumoApi = new ConsumoApi(); 
+        
+        // 2. Definimos la URL (usa tu propia API Key de OMDB)
+        var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=series&apikey=aaa426c3");
+        
+        // 3. Imprimimos el resultado que viene de internet
+        System.out.println(json); 
+    }
 }
